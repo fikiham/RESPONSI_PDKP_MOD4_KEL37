21363b854ed64b4436c7a1abbbc7c8598da17093
Abdullah Faqih Al Mubarak <82459624+fikiham@users.noreply.github.com>
2021-04-23 14:06:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+from decrypt import decrpytText, encryptText
+
 basicAlphabet = "abcdefghijklmnopqrstuvwxyz" \
+                "abcdefghijklmnopqrstuvwxyz" \
                 "abcdefghijklmnopqrstuvwxyz"
+
 newString = ""
-sumChanged = 37
-oriText = input("Original Text: ")
-print("Ori text: ", oriText)
-for i in range(len(oriText)):
-    if not oriText[i].isalpha():
-        newString += oriText[i]
-    else:
-        newString = newString + basicAlphabet[basicAlphabet.index(oriText[i]) + sumChanged]
-print("newstring :", newString)
+
+originalText = input("Original Text: ")
+print("Ori text: ", originalText)
+
+choice = str(input("1. Encrypt or 2.Decrypt :"))
+if choice == "Encrypt" or choice == "1":
+    sumChanged = 37
+    encryptText(originalText, basicAlphabet, newString, sumChanged)
+elif choice == "Decrypt" or choice == "2":
+    sumChanged = -37
+    decrpytText(originalText, basicAlphabet, newString, sumChanged)
+else:
+    print("Your choice sucks, restart the program")
