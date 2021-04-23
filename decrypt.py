8143ef3296ab6dc53da9812d72bee8915c4300ec
Abdullah Faqih Al Mubarak <82459624+fikiham@users.noreply.github.com>
2021-04-23 15:30:19 +0700

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -24,13 +24,3 @@ def decrpytText(oriText,basicAlphabet,newString, sumChanged):
         else:
             newString = newString + basicAlphabet[basicAlphabet.index(oriText[i]) + sumChanged]
     print("\nKalimat kamu berhasil di dekripsikan menjadi:", newString)
-
-def encryptText (oriText,basicAlphabet,newString, sumChanged):
-    for i in range(len(oriText)):
-        if not oriText[i].isalpha():
-            newString += oriText[i]
-        elif oriText[i].isupper():
-            newString = newString + basicAlphabet[basicAlphabet.index(oriText[i].lower()) + sumChanged].upper()
-        else:
-            newString = newString + basicAlphabet[basicAlphabet.index(oriText[i]) + sumChanged]
-    print("\nKalimat kamu berhasil di enkripsikan menjadi:", newString)
