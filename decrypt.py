eaf92e36e33d69ed5c22181baf59482135c0e336
Abdullah Faqih Al Mubarak <82459624+fikiham@users.noreply.github.com>
2021-04-23 14:17:28 +0700

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -2,6 +2,8 @@ def decrpytText(oriText,basicAlphabet,newString, sumChanged):
     for i in range(len(oriText)):
         if not oriText[i].isalpha():
             newString += oriText[i]
+        elif oriText[i].isupper():
+            newString = newString + basicAlphabet[basicAlphabet.index(oriText[i].lower()) + sumChanged].upper()
         else:
             newString = newString + basicAlphabet[basicAlphabet.index(oriText[i]) + sumChanged]
     print("newstring :", newString)
@@ -10,6 +12,8 @@ def encryptText (oriText,basicAlphabet,newString, sumChanged):
     for i in range(len(oriText)):
         if not oriText[i].isalpha():
             newString += oriText[i]
+        elif oriText[i].isupper():
+            newString = newString + basicAlphabet[basicAlphabet.index(oriText[i].lower()) + sumChanged].upper()
         else:
             newString = newString + basicAlphabet[basicAlphabet.index(oriText[i]) + sumChanged]
     print("newstring :", newString)
