cbbe9e62fb0fbe52bbd3213ded3a261187ddf66d
Felisiana Ardelia Azzahra <42663389+felisianarz@users.noreply.github.com>
2021-04-23 14:31:29 +0700

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,3 +1,19 @@
+class iden:
+    def __init__(iden, kelompok, shift):
+        iden.kelompok = kelompok
+        iden.shift = shift
+    def identitas(iden):
+        print(f"Kelompok = {iden.kelompok}\nShift = {iden.shift}")
+
+class nama:
+    def __init__(nama, nama1,nama2,nama3,nama4):
+        nama.nama1 = nama1
+        nama.nama2 = nama2
+        nama.nama3 = nama3
+        nama.nama4 = nama4
+    def anggota(nama):
+        print(f"Anggota 1 = {nama.nama1}\nAnggota 2 = {nama.nama2}\nAnggota 3 = {nama.nama3}\nAnggota 4 = {nama.nama4}")
+
 def decrpytText(oriText,basicAlphabet,newString, sumChanged):
     for i in range(len(oriText)):
         if not oriText[i].isalpha():
@@ -6,7 +22,7 @@ def decrpytText(oriText,basicAlphabet,newString, sumChanged):
             newString = newString + basicAlphabet[basicAlphabet.index(oriText[i].lower()) + sumChanged].upper()
         else:
             newString = newString + basicAlphabet[basicAlphabet.index(oriText[i]) + sumChanged]
-    print("newstring :", newString)
+    print("Newstring :", newString)
 
 def encryptText (oriText,basicAlphabet,newString, sumChanged):
     for i in range(len(oriText)):
@@ -16,5 +32,4 @@ def encryptText (oriText,basicAlphabet,newString, sumChanged):
             newString = newString + basicAlphabet[basicAlphabet.index(oriText[i].lower()) + sumChanged].upper()
         else:
             newString = newString + basicAlphabet[basicAlphabet.index(oriText[i]) + sumChanged]
-    print("newstring :", newString)
-
+    print("Newstring :", newString)
